733c06d1339d78dd9e06aa6c15f4b444f818d55c
Pawan Kumar <pawan221b@gmail.com>
2018-05-20 17:27:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutterbackdrop/twopanels.dart';
+
+void main() => runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(primarySwatch: Colors.teal),
+      home: new BackdropPage(),
+    ));
+
+class BackdropPage extends StatefulWidget {
+  @override
+  _BackdropPageState createState() => new _BackdropPageState();
+}
+
+class _BackdropPageState extends State<BackdropPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 100), value: 1.0);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
+
+  bool get isPanelVisible {
+    final AnimationStatus status = controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Backdrop"),
+        elevation: 0.0,
+        leading: new IconButton(
+          onPressed: () {
+            controller.fling(velocity: isPanelVisible ? -1.0 : 1.0);
+          },
+          icon: new AnimatedIcon(
+            icon: AnimatedIcons.close_menu,
+            progress: controller.view,
+          ),
+        ),
+      ),
+      body: new TwoPanels(
+        controller: controller,
+      ),
+    );
+  }
+}
---
+++ lib/twopanels.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class TwoPanels extends StatefulWidget {
+  final AnimationController controller;
+
+  TwoPanels({this.controller});
+
+  @override
+  _TwoPanelsState createState() => new _TwoPanelsState();
+}
+
+class _TwoPanelsState extends State<TwoPanels> {
+  static const header_height = 32.0;
+
+  Animation<RelativeRect> getPanelAnimation(BoxConstraints constraints) {
+    final height = constraints.biggest.height;
+    final backPanelHeight = height - header_height;
+    final frontPanelHeight = -header_height;
+
+    return new RelativeRectTween(
+            begin: new RelativeRect.fromLTRB(
+                0.0, backPanelHeight, 0.0, frontPanelHeight),
+            end: new RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0))
+        .animate(new CurvedAnimation(
+            parent: widget.controller, curve: Curves.linear));
+  }
+
+  Widget bothPanels(BuildContext context, BoxConstraints constraints) {
+    final ThemeData theme = Theme.of(context);
+
+    return new Container(
+      child: new Stack(
+        children: <Widget>[
+          new Container(
+            color: theme.primaryColor,
+            child: new Center(
+              child: new Text(
+                "Back Panel",
+                style: new TextStyle(fontSize: 24.0, color: Colors.white),
+              ),
+            ),
+          ),
+          new PositionedTransition(
+            rect: getPanelAnimation(constraints),
+            child: new Material(
+              elevation: 12.0,
+              borderRadius: new BorderRadius.only(
+                  topLeft: new Radius.circular(16.0),
+                  topRight: new Radius.circular(16.0)),
+              child: new Column(
+                children: <Widget>[
+                  new Container(
+                    height: header_height,
+                    child: new Center(
+                      child: new Text(
+                        "Shop Here",
+                        style: Theme.of(context).textTheme.button,
+                      ),
+                    ),
+                  ),
+                  new Expanded(
+                    child: new Center(
+                      child: new Text("Front Panel",
+                          style: new TextStyle(
+                              fontSize: 24.0, color: Colors.black)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new LayoutBuilder(
+      builder: bothPanels,
+    );
+  }
+}
